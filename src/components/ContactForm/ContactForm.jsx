cb46159e77e329988f92dcec7d11f0cd157256e0
Alexuber <alexuberbuber@gmail.com>
2023-02-16 16:52:26 +0200

    adding newContact - ok

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,42 +1,76 @@
+import { Component } from 'react';
 import { nanoid } from 'nanoid';
 import styles from './ContactsForm.module.scss';
 
-export const ContactForm = ({ handleFormSubmit, handleChange }) => {
-  const userNameId = nanoid();
-  const userTelId = nanoid();
-  return (
-    <>
-      <form className={styles.form} onSubmit={handleFormSubmit}>
+const INITIAL_STATE = {
+  name: '',
+  number: '',
+};
+
+export class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleChange = evt => {
+    const { name, value } = evt.target;
+    this.setState({ [name]: value });
+  };
+
+  handleFormSubmit = e => {
+    e.preventDefault();
+    const { name, number } = this.state;
+
+    const newContact = { id: nanoid(), name, number };
+
+    this.props.addNewContact(newContact);
+
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ ...INITIAL_STATE });
+  };
+
+  render() {
+    const userNameId = nanoid();
+    const userTelId = nanoid();
+
+    return (
+      <form className={styles.form} onSubmit={this.handleFormSubmit}>
         <label className={styles.label} htmlFor={userNameId}>
           Name
         </label>
         <input
           className={styles.input}
-          onChange={handleChange}
+          onChange={this.handleChange}
           id={userNameId}
           type="text"
           name="name"
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
+          value={this.state.name}
         />
         <label className={styles.label} htmlFor={userTelId}>
           Number
         </label>
         <input
           className={styles.input}
-          onChange={handleChange}
+          onChange={this.handleChange}
           id={userTelId}
           type="tel"
           name="number"
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
+          value={this.state.number}
         />
         <button className={styles.btn} type="submit">
           Add contact
         </button>
       </form>
-    </>
-  );
-};
+    );
+  }
+}
--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,14 +1,8 @@
 import { Component } from 'react';
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import { ContactList } from 'components/ContactsList/ContactsList';
-import { nanoid } from 'nanoid';
 import { Filter } from 'components/Filter/Filter';
 import styles from './Phonebook.module.scss';
-
-const INITIAL_STATE = {
-  name: '',
-  number: '',
-};
 
 export class Phonebook extends Component {
   state = {
@@ -19,30 +13,12 @@ export class Phonebook extends Component {
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
-    name: '',
-    number: '',
   };
 
-  handleChange = evt => {
-    const { name, value } = evt.target;
-    this.setState({ [name]: value });
-  };
-
-  handleFormSubmit = e => {
-    e.preventDefault();
-    const { name, number } = this.state;
-
-    const newContact = { id: nanoid(), name, number };
-
-    this.setState(prevState => {
-      return { contacts: [...prevState.contacts, newContact] };
-    });
-
-    this.reset();
-  };
-
-  reset = () => {
-    this.setState({ ...this.state, ...INITIAL_STATE });
+  addNewContact = contact => {
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, contact],
+    }));
   };
 
   render() {
@@ -51,10 +27,7 @@ export class Phonebook extends Component {
     return (
       <section>
         <h1 className={styles.title}>Phonebook</h1>
-        <ContactForm
-          handleFormSubmit={this.handleFormSubmit}
-          handleChange={this.handleChange}
-        />
+        <ContactForm addNewContact={this.addNewContact} />
 
         <h2 className={styles.contactsTitle}>Contacts</h2>
         <Filter />
